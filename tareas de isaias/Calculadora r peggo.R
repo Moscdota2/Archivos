c53699b2b23737c7033c6c11f058c0b4450e569e
lmendoza <moscdota2@gmail.com>
2021-03-17 14:04:27 -0400

    Añadiendo nueva calculadora con funcion diferente

--- tareas de isaias/Calculadora r peggo.R
+++ tareas de isaias/Calculadora r peggo.R
@@ -1,22 +1,41 @@
-funcion_calculadora <- function(n1, n2, n3){
-  if(n3 == 'suma'){
-    suma <- (n1 + n2)
-    return(suma)
+funcion_calculadora <- function(n1, n2, comp){
+  if(comp == 'suma'){
+    res <- (n1 + n2)
+    return(res)
   } 
-  if (n3 == 'resta'){
-    resta <- (n1 - n2)
-    return(resta)
+  if (comp == 'resta'){
+    res <- (n1 - n2)
+    return(res)
   } 
-  if (n3 == 'multiplicacion'){
-    multiplicacion <- (n1 * n2)
-    return(multiplicacion)
+  if (comp == 'multiplicacion'){
+    res <- (n1 * n2)
+    return(res)
   } 
-  if (n3 == 'division'){
-    division <- (n1 / n2)
-    return(division)
+  if (comp == 'division'){
+    res <- (n1 / n2)
+    return(res)
   } 
- return(n3)
+
+  return(res)
+ 
 }
-funcion_calculadora(55,2, 'division')
+
+calcular <- function(x, y, operacion1,  operacion2 = NA){
+  if(!is.na(x) & !is.na(y) & !is.na(operacion1)){
+     resultado <- funcion_calculadora(x,y,operacion1)
+    if(!is.na(operacion2)){
+      resultado <- funcion_calculadora(resultado, nuevo_valor, operacion2)
+    }
+    resultado
+    } else {
+    print('ingrese datos correctos')
+    resultado <- NA
+  }
+ return(resultado)
+}
+
+
+calcular(x = 10, y = 10, operacion1 =  'resta', nuevo_valor =  10, operacion2 =  'suma')
+
 
 
