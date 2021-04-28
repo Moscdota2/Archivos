2ea5fd405c32aa3e8d2aaa02c274dfb17f524cc1
lmendoza <moscdota2@gmail.com>
2021-04-28 16:15:07 -0400

    Arreglando el script para arreglar la funcionalidad

--- python1/condicionales/ejercicio5.py
+++ python1/condicionales/ejercicio5.py
@@ -1,3 +1,8 @@
 edad = int(input('Ingrese su edad: '))
 ingresos = float(input('Ponga sus ingresos anuales: '))
 
+if edad > 16 and ingresos >= 1000:
+    print('Usted es apto para tributar')
+else:
+    print('Usted no es apto para tributar aún')
+
