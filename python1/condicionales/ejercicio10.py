9ab3a70a0dc29169d204fefca4a8fe69f9eabf38
lmendoza <moscdota2@gmail.com>
2021-04-29 10:36:45 -0400

    Añadiendo un ejercicio bien complicado, en un script mi lógica, y el el otro _r la respuesta completa de la página

---
+++ python1/condicionales/ejercicio10.py
@@ -0,0 +1,8 @@
+pregunta = input('Quiere una pizza vegetariana?: ')
+
+if pregunta == 'si' :
+    print('Menu: Pimiento y Tofu además de la mozzarella y el tomate')
+elif pregunta == 'no' :
+    print('Menu: Peperoni, Jamón y Salmón además de la mozzarella y el tomate')
+else:
+    print('Introduce si o no, idiota')
