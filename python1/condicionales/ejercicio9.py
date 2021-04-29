316f28c1af3556508797666c702e957452811647
lmendoza <moscdota2@gmail.com>
2021-04-29 10:28:04 -0400

    Añadiendo nuevo ejercicio de condicional que estuvo facil

---
+++ python1/condicionales/ejercicio9.py
@@ -0,0 +1,10 @@
+edad = int(input('Ingrese su edad: '))
+
+if edad < 4:
+    print('Puede entrar gratis')
+elif edad > 4 and edad < 18:
+    print('La entrada es 5 euros peggo')
+else:
+    print('La entrada es 10 euros peggito')
+
+#este si salió burda de criminal peggito
