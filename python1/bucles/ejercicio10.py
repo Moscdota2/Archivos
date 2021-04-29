6e86b3fb2e458f492772f8a96e5234adeee78c0a
lmendoza <moscdota2@gmail.com>
2021-04-29 17:04:58 -0400

    Añadiendo 10 ejercicio, ojo: no quiere correr

---
+++ python1/bucles/ejercicio10.py
@@ -0,0 +1,17 @@
+# num = int(input('Ingresa el numero: '))
+# i = 2
+# while num % i != 0:
+#     i += 1
+# if i == num:
+#     print(str(num) + 'Es primo')
+# else:
+#     print(str(num + 'No es primo')
+
+num = int(input('Ingresa el numero: '))
+i = 2
+while num % i != 0:
+    i += 1
+if i == num:
+    print(str(num) + 'Es primo')
+else:
+    print(str(num + 'No es primo')
