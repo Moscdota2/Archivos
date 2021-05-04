3ae61b041e8a46e14796e270e24edb2b7f4ab348
lmendoza <moscdota2@gmail.com>
2021-05-04 13:31:54 -0400

    Añadiendo una funcion para el llamado de la data de la base de datos

--- v3_comunicaciones_borrador/funcion_sql.R
+++ v3_comunicaciones_borrador/funcion_sql.R
@@ -1,8 +1,10 @@
-library(RPostgreSQL)
-library(DBI)
+funcion_sql <- function(){
+  sqlcomando <- "select obpp_situr_name, code, obpp_name, state_proyect from app
+  where state_proyect='Borrador' or state_proyect='En evaluación' or state_proyect='Pre-evaluación';"
+  
+  conexion <- dbConnect(PostgreSQL(), user = 'postgres', password = '040200', dbname = 'postgres')
+  out2 <- dbGetQuery(conexion, sqlcomando)
+  desconexion <- dbDisconnect(conexion)
 
-sqlcomando <- 'select * from app;'
-conexion <- dbConnect(PostgreSQL(), user = 'postgres', password = '040200', dbname = 'postgres')
-data <- dbGetQuery(conexion, sqlcomando)
-
-desconexion <- dbDisconnect(conexion)
+  return(out2)
+}
