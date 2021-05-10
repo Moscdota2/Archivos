292cdf90e9c84879fe99d419f29df8e0e4c60fd2
yangel romero <yangelromero7@gmail.com>
2021-05-10 16:11:18 -0400

    Añadiendo script que mi negrito yangel me pasó

---
+++ index.php
@@ -0,0 +1,43 @@
+
+<?php
+    $hola = 'hola mund';
+    echo $hola;
+    
+    if ($hola <> 'hola mundo'){
+
+        echo ' leopoldo';
+    }
+
+    $array = ["hola","como","que"];
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=
+    , initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Hola yangel</h1>
+
+    <ul>
+    <?php
+    
+    echo "<" 
+
+    foreach $array as $valor{
+
+
+    }
+    
+    ?>
+    </ul>
+    
+</body>
+</html>
