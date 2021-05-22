b6f6d747c3dfbc89b8621c9ef24cd60da1c68e14
lmendoza <moscdota2@gmail.com>
2021-05-22 18:23:59 -0400

    Para hacer el pull de mierda

--- v3_comunicaciones_borrador/funcion_sql.R
+++ v3_comunicaciones_borrador/funcion_sql.R
@@ -1,8 +1,7 @@
-library(RPostgreSQL)
 library(DBI)
+library(RPostgreSQL)
 
 sqlcomando <- 'select * from app;'
 conexion <- dbConnect(PostgreSQL(), user = 'postgres', password = '040200', dbname = 'postgres')
 data <- dbGetQuery(conexion, sqlcomando)
-
 desconexion <- dbDisconnect(conexion)
