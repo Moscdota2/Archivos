061907d7612ab526a0d736850656af2efcfde555
lmendoza <moscdota2@gmail.com>
2021-05-27 13:53:03 -0400

    Añadiendo escri

---
+++ python1/UniversidadPython/edad.py
@@ -0,0 +1,10 @@
+edad = int(input('Ingresa tu edad:'))
+
+if edad >= 0 and edad <= 10:
+    print('La infancia es increible...')
+elif edad >= 11 and edad <= 20:
+    print('Muchos cambios y mucho estudio')
+elif edad >= 21 and edad <= 30:
+    print('Amor y comienza el trabajo')
+else:
+    print('Etapa de vida no reconocida')           
