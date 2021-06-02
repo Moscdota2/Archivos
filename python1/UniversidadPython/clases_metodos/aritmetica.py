060a94b7ec71506d7ea693d5b43e39342819618e
lmendoza <moscdota2@gmail.com>
2021-06-02 10:53:36 -0400

    Añadiendo mas ejercicios de clases a mi repositorio de github

---
+++ python1/UniversidadPython/clases_metodos/aritmetica.py
@@ -0,0 +1,14 @@
+class Aritmetica:
+    """
+    Clase para practicar peggo
+    """
+
+    def __init__(self, operandoA, operandoB):
+        self.operandoA = operandoA
+        self.operandoB= operandoB
+
+    def sumar(self):
+        return self.operandoA + self.operandoB
+
+aritmetica1 = Aritmetica(5,3)
+print(aritmetica1.sumar())
