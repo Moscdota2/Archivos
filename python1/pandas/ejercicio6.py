73d60693c393ea889d28317d95d4181b9128644f
lmendoza <moscdota2@gmail.com>
2021-06-09 15:00:47 -0400

    Ni idea xd

--- python1/pandas/ejercicio6.py
+++ python1/pandas/ejercicio6.py
@@ -2,8 +2,6 @@ import pandas as pd
 
 data = pd.read_csv('/home/analista/Escritorio/cotizacion.csv')
 
-
-
 def resumen_cotizaciones(fichero):
     df = pd.read_csv(fichero, sep=';', decimal=',', thousands='.', index_col=0)
     return pd.DataFrame([df.min(), df.max(), df.mean()], index=['Mínimo', 'Máximo', 'Media'])
