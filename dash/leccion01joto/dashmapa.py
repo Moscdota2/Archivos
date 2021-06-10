7803b2a78e2334c50c7ef8add3bf0d99a6ae64f0
lmendoza <moscdota2@gmail.com>
2021-06-10 14:52:51 -0400

    mapa

---
+++ dash/leccion01joto/dashmapa.py
@@ -0,0 +1,29 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+
+import plotly.express as px
+import pandas as pd
+
+import plotly.graph_objects as go
+import pandas as pd
+us_cities = pd.read_csv("https://raw.githubusercontent.com/plotly/datasets/master/us-cities-top-1k.csv")
+
+import plotly.express as px
+
+fig = px.scatter_mapbox(us_cities, lat="lat", lon="lon", hover_name="City", hover_data=["State", "Population"],
+                        color_discrete_sequence=["fuchsia"], zoom=3, height=1000)
+fig.update_layout(
+    mapbox_style="white-bg",
+    mapbox_layers=[
+        {
+            "below": 'traces',
+            "sourcetype": "raster",
+            "sourceattribution": "United States Geological Survey",
+            "source": [
+                "https://basemap.nationalmap.gov/arcgis/rest/services/USGSImageryOnly/MapServer/tile/{z}/{y}/{x}"
+            ]
+        }
+      ])
+fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+fig.show()
