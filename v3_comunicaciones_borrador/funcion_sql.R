39ef81c046af744723843bc5cdbc6a30d8a8f8e3
Leopoldo Mendoza <63051950+Moscdota2@users.noreply.github.com>
2024-08-06 16:29:06 -0400

    Delete v3_comunicaciones_borrador directory

--- v3_comunicaciones_borrador/funcion_sql.R
+++
@@ -1,7 +0,0 @@
-library(DBI)
-library(RPostgreSQL)
-
-sqlcomando <- 'select * from app;'
-conexion <- dbConnect(PostgreSQL(), user = 'postgres', password = '040200', dbname = 'postgres')
-data <- dbGetQuery(conexion, sqlcomando)
-desconexion <- dbDisconnect(conexion)
